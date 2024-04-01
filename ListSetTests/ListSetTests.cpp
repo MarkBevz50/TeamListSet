0a1710d59a1decb9bb2305a29fd0f360a49fce41
Markiyan Bevz <bevzm50@gmail.com>
2024-04-01 23:21:55 +0300

    Added project with native unit test

---
+++ ListSetTests/ListSetTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../TeamListSet/set.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ListSetTests
+{
+	TEST_CLASS(ListSetTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}

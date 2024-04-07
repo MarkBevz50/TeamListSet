f4819289c8f3de4e1f1c838e7e5d6143b1f6db0e
Markiyan Bevz <bevzm50@gmail.com>
2024-04-07 22:29:11 +0300

    Added new files

--- TeamListSet/main.cpp
+++ TeamListSet/main.cpp
@@ -45,15 +45,6 @@ int main() {
     set5.Add(A);
     set5.Add(B);
     set5.Add(C);
-    set5.Print();
-    std::cout << "Enter your Number: ";
-    int number;
-    std::cin >> number;
-    std::cout << "Your number contains " << digit(number) << " different digits\n";
-   
-    
-    
-    
-    
+    set5.Print();                             
     return 0;
 }
